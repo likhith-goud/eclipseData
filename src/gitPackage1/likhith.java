5947d22bf4080102e54d115272a50368183a1c45
likhith-goud <likhithgoud1213@gmail.com>
2025-04-23 21:02:27 +0530

    pushing eclipse project to central

---
+++ src/gitPackage1/likhith.java
@@ -0,0 +1,16 @@
+package gitPackage1;
+
+public class likhith {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+	}
+
+}
